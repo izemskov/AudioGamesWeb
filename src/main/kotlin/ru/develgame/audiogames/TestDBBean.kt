42d13429b63e1c1921fd4781430e50ce75baff4b
izemskov <pascal.ilya@gmail.com>
2022-10-15 16:09:37 +0300

    TestDB connection

--- src/main/kotlin/ru/develgame/audiogames/TestDBBean.kt
+++ src/main/kotlin/ru/develgame/audiogames/TestDBBean.kt
@@ -1,16 +1,25 @@
 package ru.develgame.audiogames
 
+import ru.develgame.audiogames.entity.AudioGame
 import java.io.Serializable
 import javax.annotation.Resource
 import javax.faces.view.ViewScoped
 import javax.inject.Named
 import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+import javax.transaction.UserTransaction
 
 @Named("testdb")
 @ViewScoped
 open class TestDBBean: Serializable {
-    @Resource
+    @PersistenceContext
     private lateinit var entityManager: EntityManager
 
+    @Resource
+    private lateinit var userTransaction: UserTransaction
 
+    open fun testFun() {
+        val find = entityManager.find(AudioGame::class.java, 1L)
+        println(find.name)
+    }
 }
